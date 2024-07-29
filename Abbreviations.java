ce89b2c7a45656abfd1d7de3201979718c537bbf
GreggtheDev <greggthedev@gmail.com>
2024-07-29 14:54:23 -0400

    created class to manage common abbreviations

--- Abbreviations.java
+++ Abbreviations.java
@@ -3,3 +3,8 @@ import java.util.Map;
 
 public class Abbreviations {
     private Map<String, String> abbreviationMap;
+
+    // Constructor
+    public Abbreviations() {
+        this.abbreviationMap = new HashMap<>();
+    }
