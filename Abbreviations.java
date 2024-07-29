9b10b6b76b28fb14627798684a6c1887c21952ac
GreggtheDev <greggthedev@gmail.com>
2024-07-29 14:58:14 -0400

    added methods for adding new abbreviations, to check if abbreviation has already been added and explanation for abbreviations

--- Abbreviations.java
+++ Abbreviations.java
@@ -8,3 +8,18 @@ public class Abbreviations {
     public Abbreviations() {
         this.abbreviationMap = new HashMap<>();
     }
+
+    // Method to add a new abbreviation and its explanation
+    public void addAbbreviation(String abbreviation, String explanation) {
+        this.abbreviationMap.put(abbreviation, explanation);
+    }
+
+    // Method to check if an abbreviation has already been added
+    public boolean hasAbbreviation(String abbreviation) {
+        return this.abbreviationMap.containsKey(abbreviation);
+    }
+
+    // Method to find the explanation for an abbreviation
+    public String findExplanationFor(String abbreviation) {
+        return this.abbreviationMap.getOrDefault(abbreviation, null);
+    }
