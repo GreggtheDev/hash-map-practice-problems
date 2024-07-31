38f1a2e94e000681c3bac22130da5faf08c08a45
GreggtheDev <greggthedev@gmail.com>
2024-07-30 20:14:25 -0400

    added methods which prints all the values in the hashmap given as a parameter using the toString method of the Book objects.

--- Program.java
+++ Program.java
@@ -27,6 +27,22 @@ public class Program {
         }
     }
 
+    // Method to print all values in the given hashmap of Books
+    public static void printValues(HashMap<String, Book> hashmap) {
+        for (Book book : hashmap.values()) {
+            System.out.println(book);
+        }
+    }
+
+    // Method to print books in the given hashmap which name contains the specified text
+    public static void printValueIfNameContains(HashMap<String, Book> hashmap, String text) {
+        for (Book book : hashmap.values()) {
+            if (book.getName().contains(text)) {
+                System.out.println(book);
+            }
+        }
+    }
+
     // Main method for testing the implemented methods
     public static void main(String[] args) {
         HashMap<String, String> sampleMap = new HashMap<>();
@@ -43,5 +59,15 @@ public class Program {
 
         System.out.println("\nPrinting values of keys containing 'ap':");
         printValuesOfKeysWhere(sampleMap, "ap");
+
+        HashMap<String, Book> bookMap = new HashMap<>();
+        bookMap.put("Sense and Sensibility", new Book("Sense and Sensibility", 1811, "..."));
+        bookMap.put("Pride and Prejudice", new Book("Pride and Prejudice", 1813, "...."));
+
+        System.out.println("\nPrinting all book values:");
+        printValues(bookMap);
+
+        System.out.println("\nPrinting books with names containing 'Pride':");
+        printValueIfNameContains(bookMap, "Pride");
     }
 }
