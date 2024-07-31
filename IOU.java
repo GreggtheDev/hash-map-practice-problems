49479e271b7eaab43577d758e4173ff9f635553a
GreggtheDev <greggthedev@gmail.com>
2024-07-30 20:29:44 -0400

    added method for testing IOU CLASS

--- IOU.java
+++ IOU.java
@@ -23,3 +23,19 @@ public class IOU {
         // If the person is not found, return 0.0
         return this.debts.getOrDefault(toWhom, 0.0);
     }
+
+    // Main method for testing the IOU class
+    public static void main(String[] args) {
+        // Create a new IOU instance
+        IOU mattsIOU = new IOU();
+
+        // Set the amounts owed to Arthur and Michael
+        mattsIOU.setSum("Arthur", 51.5);
+        mattsIOU.setSum("Michael", 30);
+
+        // Print the amounts owed to Arthur and Michael
+        // Expected output: 51.5 for Arthur and 30 for Michael
+        System.out.println(mattsIOU.howMuchDoIOweTo("Arthur"));
+        System.out.println(mattsIOU.howMuchDoIOweTo("Michael"));
+    }
+}
