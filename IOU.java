b44a416ae9af394486308514ebcccef380be314b
GreggtheDev <greggthedev@gmail.com>
2024-07-30 20:23:38 -0400

    constructor to create new IOU instance and initialize the debts map

--- IOU.java
+++ IOU.java
@@ -4,3 +4,9 @@ import java.util.Map;
 public class IOU {
     // Map to store the amounts owed to each person
     private Map<String, Double> debts;
+
+    // Constructor to create a new IOU instance
+    public IOU() {
+        // Initialize the debts map
+        this.debts = new HashMap<>();
+    }
